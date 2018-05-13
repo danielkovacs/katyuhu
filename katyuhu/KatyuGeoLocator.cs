8b2fe0dc45cb8bee72e701d0d1805463b79ddaef
Daniel Kovacs <danielk@apsolution.com.au>
2018-05-13 22:26:42 +0200

    Location and address with a found package

---
+++ katyuhu/KatyuGeoLocator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+
+namespace katyuhu.Droid
+{
+	public class KatyuGeoLocator
+	{
+        
+		private IGeolocator locator = CrossGeolocator.Current;
+
+		public Address address;
+		 
+		public KatyuGeoLocator()
+		{
+		}
+
+		public async Task<Position> GetCurrentLocation()
+		{
+			Position position = null;
+			try
+			{
+				locator.DesiredAccuracy = 100;
+                
+				position = await locator.GetLastKnownLocationAsync();
+
+
+				if (position != null)
+				{
+					//got a cahched position, so let's use it.
+					IEnumerable<Address> adrs = await locator.GetAddressesForPositionAsync(position);
+					this.address = adrs.ToList().FirstOrDefault<Address>();
+					return position;
+
+				}
+				position = await locator.GetPositionAsync(TimeSpan.FromSeconds(20), null, true);
+				IEnumerable<Address> ads = await locator.GetAddressesForPositionAsync(position);
+				this.address = ads.ToList().FirstOrDefault<Address>();
+
+                
+			}
+
+			catch (Exception ex)
+			{
+				//Display error as we have timed out or can't get location.
+				Console.WriteLine(ex);
+			}
+
+			if (position == null)
+				return new Position();
+
+			var output = string.Format("Time: {0} \nLat: {1} \nLong: {2} \nAltitude: {3} \nAltitude Accuracy: {4} \nAccuracy: {5} \nHeading: {6} \nSpeed: {7}",
+				position.Timestamp, position.Latitude, position.Longitude,
+				position.Altitude, position.AltitudeAccuracy, position.Accuracy, position.Heading, position.Speed);
+
+
+
+			Debug.WriteLine(output);
+			return position;
+		}
+	}
+}
--- katyuhu/MainActivity.cs
+++ katyuhu/MainActivity.cs
@@ -10,14 +10,14 @@ namespace katyuhu.Droid
     {
         int count = 1;
 
-        string connStr = "server=TODO;user=root;database=TODO;port=3306;password=Lofasz2";
+        string connStr = "server=katyu.hu;user=root;database=Katyuhu;port=3306;password=Lofasz2";
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             DBConnector connector = new DBConnector(connStr);
-
+			KatyuGeoLocator locator = new KatyuGeoLocator();
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
@@ -26,7 +26,12 @@ namespace katyuhu.Droid
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            button.Click += delegate { 
+				button.Text = $"{count++} clicks!";
+				var loc = System.Threading.Tasks.Task.Run( () => locator.GetCurrentLocation()).Result;
+				button.Text = $"Your current location is : LAT:{loc.Latitude},LON:{loc.Longitude}";
+				button.Text += $"Address:{locator.address}";
+			};
         }
     }
 }
